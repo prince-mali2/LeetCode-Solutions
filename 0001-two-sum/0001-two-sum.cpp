f457dc99aa0f97238b3d8e500185b8b86fe2e206
Prince Mali <princemali019@gmail.com>
2025-07-14 23:14:47 +0530

    Time: 2 ms (75.93%), Space: 16 MB (6.87%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
+        int n = nums.size();
+        vector<int> ans;
         unordered_map<int,int> mp;
-
-        for(int i=0;i<nums.size();i++){
-            int diff = target - nums[i];
-            if(mp.count(diff)){
-                return {mp[diff], i};
-                
-            }
+        for(int i=0;i<n;i++){
             mp[nums[i]]=i;
         }
-        return {};
+
+        for(int i=0;i<n;i++){
+            int diff = target-nums[i];
+
+            if(mp.find(diff)!=mp.end() && mp[diff]!=i){
+                ans.push_back(i);
+                ans.push_back(mp[diff]);
+                break;
+            }
+        }
+        return ans;
     }
 };
