4cec169f33ff31711353aa7d89c81bcf180cce14
Prince Mali <princemali019@gmail.com>
2025-07-27 00:43:25 +0530

    Time: 12 ms (12.86%), Space: 20.8 MB (26.16%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -7,8 +7,8 @@ public:
         sort(nums.begin(), nums.end());
         for(int i=0;i<n;i++){
             if(i==0 && nums[i]!=nums[i+1]) return nums[i];
-            else if(i==n-1 && nums[i]!=nums[i-1]) return nums[i];
             else if(i>0 && i<n &&nums[i]!=nums[i-1] && nums[i]!=nums[i+1])return nums[i];
+            else if(i==n-1 && nums[i]!=nums[i-1]) return nums[i];
 
 
  
