4ca3635df7a5a920f7e6a1fc96291b289461f8ec
Prince Mali <princemali019@gmail.com>
2025-09-29 23:18:06 +0530

    Time: 0 ms (100%), Space: 87.1 MB (93.59%) - LeetHub

---
+++ 0890-lemonade-change/0890-lemonade-change.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+
+        int n=bills.size();
+        int five=0;
+        int ten=0;
+        for(int i=0;i<n;i++){
+            if(bills[i]==5){
+                five++;
+            }
+            else if(bills[i]==10){
+                ten++;
+                if(five>=1)five-=1;
+                else return false;
+            }
+            else{
+                if(ten>0 && five>0){
+                    ten--;
+                    five--;
+                }
+                else if(five>=3) five-=3;
+                else return false;
+            }
+        }
+        return true;
+    }
+};
