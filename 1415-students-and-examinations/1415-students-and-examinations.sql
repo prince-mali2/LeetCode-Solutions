b7696fafcf10115b1e60e4f8e5a0a730e5218142
Prince Mali <princemali019@gmail.com>
2025-10-07 18:53:12 +0530

    Time: 952 ms (48.12%), Space: 0B (100%) - LeetHub

---
+++ 1415-students-and-examinations/1415-students-and-examinations.sql
@@ -0,0 +1,18 @@
+SELECT 
+  s.student_id, 
+  s.student_name, 
+  ss.subject_name, 
+  COUNT(e.student_id) AS attended_exams
+FROM Students s
+CROSS JOIN Subjects ss
+LEFT JOIN Examinations e
+  ON s.student_id = e.student_id 
+  AND ss.subject_name = e.subject_name
+GROUP BY 
+  s.student_id, 
+  s.student_name, 
+  ss.subject_name
+ORDER BY 
+   s.student_id, 
+  s.student_name, 
+  ss.subject_name;
