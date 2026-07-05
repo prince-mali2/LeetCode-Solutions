be18b6fd268ad734cc340e59f8d45ec49616cd09
Prince Mali <princemali019@gmail.com>
2026-07-05 23:36:03 +0530

    Time: 2 ms (42.02%), Space: 94.6 MB (15.32%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        int buy = prices[0];
+        int sell=prices[0];
+        int profit = 0 ;
+
+        for(int i=1; i<prices.length; i++){
+            if(prices[i]<buy) buy=prices[i];
+            
+            else{
+                sell = prices[i];
+                 profit = Math.max(profit , sell -buy);
+            }
+        }
+        
+        return profit;
+    }
+}
