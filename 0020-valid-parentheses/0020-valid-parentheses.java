a54734874b3e60d532c62f9fc6e02f6729113f66
Prince Mali <princemali019@gmail.com>
2026-07-26 12:27:00 +0530

    Time: 4 ms (37.37%), Space: 43.2 MB (58.1%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for(char c : s.toCharArray()){
+            if(c=='(' || c=='{' || c=='[')st.push(c);
+            else  {
+                if(st.empty()) return false;
+
+                if(c==')' && st.pop() !='(') return false;
+                else if(c=='}' && st.pop() !='{') return false;
+                else if(c==']' && st.pop() !='[') return false;
+            }
+        }
+        return st.empty() ;
+    }
+}
